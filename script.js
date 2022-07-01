a19f8f9d942fe579c28b3181f12c8f3bab80ebc0
aymanelagli <108526293+aymanelagli@users.noreply.github.com>
2022-07-01 12:34:08 +0100

    added new files

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+var typed = new Typed(".typing",{
+  strings:["Web Developer","Web Designer","Graphic Designer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+}
+
+
+
+
+)
